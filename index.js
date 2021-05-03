5a4481b6952031ec12eb53e1dd678028c39b37e6
Dylan <chapuis.dylan18@gmail.com>
2021-05-03 19:35:01 +0200

    Fin de l'atelier

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+const {
+    Input
+} = require("postcss");
+
 var click = 0;
 
 function ClickCounter() {
@@ -5,15 +9,25 @@ function ClickCounter() {
 }
 
 function changeColor(x, y, z) {
-    
-    if (click % 2 == 0){
+
+    if (click % 2 == 0) {
         document.getElementById("button").style.background = x;
-    }
-    else if (click % 5 == 0){
+    } else if (click % 5 == 0) {
         document.getElementById("button").style.background = z;
-    }
-    else {
+    } else {
         document.getElementById("button").style.background = y;
     }
-    
+
+};
+
+function Validate() {
+    var name_entered = document.getElementById("nom")
+    if (!name_entered.checkValidity()) {
+        document.getElementById('false').innerHTML = 'Wrong name !';
+    } else {
+        document.getElementById('false').innerHTML = '';
+        var name_entered = document.getElementById("nom").value;
+        alert("Bonjour" + " " + name_entered + " !");
+
+    }
 }
